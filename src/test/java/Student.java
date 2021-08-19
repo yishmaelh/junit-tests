5768362c4a17c7a6d4ec91edc09d5bc0150829af
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-19 11:15:23 -0500

    get gradeAverage

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class Student {
 
     private String name;
     private long studentId;
+    private ArrayList<Integer> grades;
+
 
     public Student(String name, long studentId) {
         this.name = name;
@@ -16,4 +20,20 @@ public class Student {
         return name;
     }
 
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
+
+    public double getGradeAverage(){
+        double sumOfPoints = 0;
+        double numberOfGrades = grades.size();
+        for (Integer grade : grades){
+            sumOfPoints += grade;
+        }
+        return sumOfPoints/numberOfGrades;
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -11,4 +11,6 @@ public class StudentTest {
     assertEquals(studentId, studentId);
     }
 
+
+
 }
