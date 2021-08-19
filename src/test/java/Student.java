f49b02f541bbc5d8c2be1c62082dac52c128edd5
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-19 10:37:55 -0500

    constructor

---
+++ src/test/java/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+    public String name;
+    public long studentId;
+
+    public Student(String name, long studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+}
